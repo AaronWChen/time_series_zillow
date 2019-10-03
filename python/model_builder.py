26c09e2973e1cf3badbae71692125aefb357392b
Henok Yemam <henokadem1@gmail.com>
2019-10-03 16:36:49 -0700

    modeling predictor imporved, codes refactored

--- python/model_builder.py
+++ python/model_builder.py
@@ -6,34 +6,63 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima_model import ARMA, ARIMA
 import numpy as np
 
+def dataframe_cleaner (df):
+    """Takes in a pandas dataframe and 
+        removes all the columns with null values
+        and returns a pandas dataframe"""
+    columns = []
+    for col in df:
+        if (df[col].isnull().sum()) == 0:
+            columns.append(col)
+    return df[columns]
+
 def stationarity_test(df, conversion):
     """This function takes in a pandas dataframe
         and runs a time series stationary test. 
-        It returns a tuple name of the columns, value of
-        conversion in the case of diff() and value of
-        adfuller p-score.
+        It returns a tuple with value of
+        adfuller p-score and the n
+        conversion in the case of diff() and 
         conversion : {'pct_change', 'diff()'}"""
-    assert isinstance(df, pd.DataFrame), 'First argument needs to be a dataframe'
     assert isinstance(conversion, str), 'Second argument needs to be a string'
 
     if conversion == 'pct_change()':
         stationary_test = []
-        for col in df:
-            adf_df = adfuller(df[col].pct_change().dropna())[1]
-            stationary_test.append((col, adf_df))
+        for j in range(1,10):
+            adf_df = adfuller(df.pct_change(j).dropna())[1]
+            if adf_ < 0.05:
+                stationary_test.append((i, adf_df))
+                break 
         return stationary_test
 
     elif conversion == 'diff()':
         test_stationary = []
-        for col in df:
-            for i in range(1, 10):
-                adf_ = adfuller(df[col].diff(i).dropna())[1]
-                if adf_ < 0.05:
-                    test_stationary.append((col, i, adf_))
-                    break
+        for i in range(1, 10):
+            adf_ = adfuller(df.diff(i).dropna())[1]
+            if adf_ < 0.05:
+                test_stationary.append((i, adf_))
+                break 
         return test_stationary
 
+def model(df):
+    """Takes in a stationary pandas series and
+        gives back ARIMA model for your series"""
     
+    model_params = []
+    model_lowestbic = []
+    
+    for p in range(1,3):
+        for q in range(1,3):
+            test_model = ARIMA(df, (p,0,q)).fit()
+            test_bic = test_model.bic
+            model_params.append((p, q, test_model, test_bic))
+    for tup in model_params:
+        model_lowestbic.append(tup[3])
+        
+    lowest_bic = min(model_lowestbic)
+    for tup in model_params:
+        if tup[3]==lowest_bic:
+            return tup[2].plot_predict(1, 275)
+
 
 
 
